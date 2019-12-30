596a63c04c60fb80266e36be908da0b67e84f35f
Nicholas Villacreces <nvillacreces72@gmail.com>
2019-12-29 20:59:59 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+function clicksContact(){
+    $("#contact-button").click(function(){
+        $("body,html").animate({
+            scrollTop: $(".form-flex").offset().top
+        }, 800);
+});
+}
+
+function clicksBio(){
+
+
+    $("#bio-button").on('click', function(){
+        $('.hero-text').addClass("w3-animate-opacity");
+        $('.hero-text').css("visibility","visible");
+        
+      //  $(".hero-image").css("visibility", "hidden");
+    });
+
+}
+
+function clicksProjects(){
+
+    $("#projects-button").click(function(){
+        $("body,html").animate({
+            scrollTop: $(".project-flex").offset().top
+        }, 800);
+});
+}
+
+function clicksPopBio(){
+    
+
+    $('#pop-bio').click(function(){
+        $('.hero-text').addClass("w3-animate-opacity");
+        $('.hero-text').css("visibility","visible");
+    });
+}
+
+function clicksPopProjects(){
+    
+    $("#pop-projects").click(function(){
+        $("body,html").animate({
+            scrollTop: $(".project-flex").offset().top
+        }, 800);
+    });
+
+}
+
+function clicksPopContacts(){
+    
+    $("#pop-contacts").click(function(){
+        $("body,html").animate({
+            scrollTop: $(".form-flex").offset().top
+        }, 800);
+    });
+}
+
+
+function theApp(){
+
+    clicksBio();
+    clicksProjects();
+    clicksContact();
+    clicksPopBio();
+    clicksPopProjects();
+    clicksPopContacts();
+}
+
+$(theApp);
